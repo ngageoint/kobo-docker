03ea15a502347e21a4d1f5203bf021ad637ca5d1
sjudeng <sjudeng@users.noreply.github.com>
2016-05-06 20:02:29 +0200

    Fix geoserver data dir ownership

--- scripts/ssl_add_ca_cert.bash
+++ scripts/ssl_add_ca_cert.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -f /tmp/kobo_toolbox_secrets/rootCA.crt ] && [ ! -f /usr/share/ca-certificates/rootCA.crt ]; then
-  cp /tmp/kobo_toolbox_secrets/rootCA.crt /usr/share/ca-certificates > /dev/null 2>&1
-  echo "rootCA.crt" >> /etc/ca-certificates.conf > /dev/null 2>&1
+  cp /tmp/kobo_toolbox_secrets/rootCA.crt /usr/share/ca-certificates
+  echo "rootCA.crt" >> /etc/ca-certificates.conf
   update-ca-certificates --fresh > /dev/null 2>&1
 fi
 
